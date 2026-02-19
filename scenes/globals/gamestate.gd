927dc768f73da85114e14baef15d4d036c30591e
frog <tristoni540@gmail.com>
2026-02-18 17:01:28 -0800

    level instantiation
    created a class that hold necessary level info such as bpm, music track, anim track etc.
    
    gamestate can now instantiate these levels be being given a name from the list and then sending relevant info the the 'notemaker'

--- scenes/globals/gamestate.gd
+++ scenes/globals/gamestate.gd
@@ -1,14 +1,16 @@
 extends Node
 
-
-# song vars 
-var current_bpm := 125
-var beat_length = 60 / current_bpm # seconds
-var note_existence_length = 4 # beats - need to move this to an export of group vars
-var group_existence_length = 4 # beats
+@export var level_infos : Array[Level_Info]
+var level_dict = {}
 
 # shifts the song to start further in, nice for testing tracks
-var debug_start_time = 0.0 # seconds
+const debug_start_time = 0.0 # seconds
+
+# song vars 
+var current_bpm := 60 # default
+var beat_length = 60.0 / float(current_bpm) # seconds
+var note_existence_length = 4 # beats - need to move this to an export of group vars
+var group_existence_length = 4 # beats
 
 # storing counts of interactions
 var note_count := 0
@@ -19,17 +21,31 @@ var good_count := 0
 var okay_count := 0
 var miss_count := 0
 
-# 1. instantiate variables for a choosen song / level
-# 2. track scoring and progress of a song
-# 3. calculate results for display at the end of a song
+# 1. instantiate variables for a choosen song / level ***
+# 2. track scoring and progress of a song ***
+# 3. calculate results for display at the end of a song 
 
 func _ready():
 	_establish_events()
+	
+	for info in level_infos:
+		level_dict[info.name] = info
 
 func _establish_events():
 	Events.group_finished.connect(_group_finished)
 	Events.note_hit.connect(_note_hit)
 	Events.note_spawned.connect(_note_spawned)
+	Events.level_selected.connect(_setup_level)
+
+func _setup_level(level_name : Level_Info.LEVEL_NAME):
+	if !level_dict.has(level_name): push_error("Unable to find " + str(level_name) + "in level info class!")
+	else:
+		var level_info : Level_Info = level_dict[level_name]
+		# setup gamestate vars (more likely later)
+		current_bpm = level_info.bpm
+		beat_length = 60.0 / float(current_bpm)
+		
+		Events.level_start.emit(level_info) # sending this out from gamestate after setting vars to avoid a race condition
 
 #### counting methods
 func _note_hit(): note_hit_count += 1
---
+++ scenes/globals/level settings/Level_Info.gd
@@ -0,0 +1,13 @@
+extends Resource
+class_name Level_Info
+
+enum LEVEL_NAME { # set actual names later
+	LEVEL_1,
+	LEVEL_2,
+	LEVEL_3
+}
+
+@export var name : LEVEL_NAME
+@export var music_file : AudioStream
+@export var bpm : int
+@export var conductor_anim : String
