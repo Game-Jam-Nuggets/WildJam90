b39ff9b1c944f5079496a58e411ec5f45a699009
Kylen Xiao <ZeldaSpider599@gmail.com>
2026-02-21 16:06:24 -0700

    Added sword and arm to hyla walk animation.

--- scenes/hyla/hyla.gd
+++ scenes/hyla/hyla.gd
@@ -3,4 +3,5 @@ extends Node2D
 @export var footstep_sounds: Array[AudioStream]
 
 func play_footstep():
-	AudioManager.play_sfx(footstep_sounds.pick_random(), 0.3, randf_range(0.9, 1.1))
+	#AudioManager.play_sfx(footstep_sounds.pick_random(), 0.3, randf_range(0.9, 1.1))
+	pass
