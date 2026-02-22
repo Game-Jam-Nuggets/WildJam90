91335bb54eea64b57c70ae1b10bbb1cd6306b543
Kylen Xiao <ZeldaSpider599@gmail.com>
2026-02-21 21:04:02 -0700

    Added level select sound to level select.

--- scenes/menus/level_select.gd
+++ scenes/menus/level_select.gd
@@ -1,11 +1,13 @@
 extends Control
 
+@export var level_select_sound: AudioStream
+
 func _on_level_1_pressed() -> void:
-	AudioManager.play_ui_click()
+	AudioManager.play_sfx(level_select_sound)
 	Events.level_selected.emit(Level_Info.LEVEL_ID.LEVEL_1)
 
 func _on_level_2_pressed() -> void:
-	AudioManager.play_ui_click()
+	AudioManager.play_sfx(level_select_sound)
 	Events.level_selected.emit(Level_Info.LEVEL_ID.LEVEL_2)
 
 func _on_return_button_pressed() -> void:
